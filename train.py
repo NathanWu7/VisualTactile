6c772c1dd2eec61fe82fc095c11cb589deb9ce91
NathanWu7 <1055482789@qq.com>
2024-04-19 17:05:00 +0800

    update

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings('ignore')   #if you don't want to check
+
 from utils.config import set_np_formatting, set_seed, get_args, parse_sim_params, load_cfg
 from utils.parse_task import parse_task
 from utils.runner import Runner
@@ -12,11 +15,10 @@ from utils.runner import Runner
 def train():
     print("Algorithm: ", args.algo)
 
-    if args.algo in ["ppo", "sac", "td3", "trpo","test","ppopc","pcsampling","vta","vtp"]: 
+    if args.algo in ["ppo", "sac", "td3","test","rla","vts","vta","vtp"]: 
         task = parse_task(args, env_cfg, train_cfg, sim_params)
         runner = Runner(task, train_cfg)
-
-        if args.test == "True":
+        if args.test == True:
             runner.eval()
         else:
             runner.run()
